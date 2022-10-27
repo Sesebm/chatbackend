316336abebdf805a87dd008627de3bc627a51d8e
sesebm <sesebastis@gmail.com>
2022-10-27 17:05:04 -0500

    first commit

---
+++ project/routers/conversations.router.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+const passport = require('passport')
+//const messageService = require('../services/messages.services')
+const conversationService = require('../services/conversations.services')
+
+
+router.route('/')
+    .get(passport.authenticate('jwt', {session: false}), conversationService.getMyConversations)
+    .post(passport.authenticate('jwt', {session: false}), conversationService.createConversation)
+
+
+
+router.route('/:conversation_id')
+    .get(passport.authenticate('jwt', {session: false}),conversationService.getMyConversationById)
+    .patch(passport.authenticate('jwt', {session: false}),conversationService.updateConversation)
+    .delete(passport.authenticate('jwt', {session: false}),conversationService.deleteMyConversation)
+    
+
+module.exports = router
